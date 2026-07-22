ccbc086cf7acdf769cbe9971b0af539bb396eb12
Shakuntala Saunshi <shakuntalasaunshi@gmail.com>
2026-07-23 00:05:27 +0530

    Fix home layout and improve chat UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,13 @@
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Features from "../components/Features";
 import Stats from "../components/Stats";
-import Footer from "../components/Footer";
 
 function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
       <Features />
       <Stats />
-      <Footer />
     </>
   );
 }
